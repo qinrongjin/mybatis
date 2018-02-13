1f27fa60c1716519c9a12b6515234c928b25b2ba
qinrongjin <qinrongjin@126.com>
2018-02-13 13:22:50 +0800

    調整druid

--- src/main/java/cn/tiny77/App.java
+++ src/main/java/cn/tiny77/App.java
@@ -1,18 +1,8 @@
 package cn.tiny77;
 
-import cn.tiny77.bean.Blog;
 import cn.tiny77.mapper.BlogMapper;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 /**
  * Hello world!
